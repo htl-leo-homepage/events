87380ac9a20bc8ab6f6ca39d10086bb0d908d188
Martin Huemer <m.huemer@htl-leonding.ac.at>
2026-03-12 14:16:10 +0100

    minor fixes

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ new Splide('#kategorien-splide', {
             type: 'loop',
             perPage: 1,
             arrows: true,
-            pagination: true,
-            gap: '1rem',
+            pagination: false,
+            gap: '3rem',
+            speed: 800,
+            easing: 'cubic-bezier(0.25, 0.1, 0.25, 1)',
         }).mount();
