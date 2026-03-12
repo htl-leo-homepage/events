86831f395fbf0c4b85b27f4ebe11c732ea9b5696
Martin Huemer <m.huemer@htl-leonding.ac.at>
2026-03-12 12:18:46 +0100

    splidejs slider

---
+++ js/script.js
@@ -0,0 +1,7 @@
+new Splide('#kategorien-splide', {
+            type: 'loop',
+            perPage: 1,
+            arrows: true,
+            pagination: true,
+            gap: '1rem',
+        }).mount();
