d9ae4d8275fb3edfcd1bbe6440f08b53b8beccdf
Martin Huemer <m.huemer@htl-leonding.ac.at>
2026-03-13 12:41:05 +0100

    splide fix

--- fotowettbewerb/js/script.js
+++ fotowettbewerb/js/script.js
@@ -1,18 +1,24 @@
 
-        let splide = new Splide('#kategorien-splide', {
-    type: 'loop',
-    perPage: 1,
-    focus: 'center',
-    gap: '2rem',
-    padding: { left: '20%', right: '20%' },
-    autoplay: true,
-    interval: 20000,
-    arrows: true,
-    pagination: false,
-    pauseOnHover: false,
-    speed: 500,
-    drag: true,
-    swipe: true,
-  });
+let splide = new Splide('#kategorien-slider', {
+	type: 'loop',
+	perPage: 1,
+	focus: 'center',
+	gap: '2rem',
+	padding: { left: '20%', right: '20%' },
+	autoplay: true,
+	interval: 20000,
+	arrows: true,
+	pagination: false,
+	pauseOnHover: false,
+	speed: 500,
+	drag: true,
+	swipe: true,
+	breakpoints: {
+		768: {
+			perPage: 1,
+			padding: { left: '0', right: '0' },
+		}
+	},
+});
 
-  splide.mount();
+splide.mount();
